032d3a179696c2ed6773275d764d83de4c4dff79
Neuzcore <ChristophNeurath@proton.me>
2026-06-05 17:01:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cloud-Worker-Requests (workers.dev) gehen immer direkt ans Netzwerk.
  */
 
-const CACHE = 'kadenz-v6';
+const CACHE = 'kadenz-v7';
 const PRECACHE = [
   './',
   './index.html',
